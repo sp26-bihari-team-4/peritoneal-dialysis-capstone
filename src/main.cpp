3b7664382ce46c74e5ee17f7e248b7c50d4df1c0
Dennis Zhitenev <denniszhitenev@gmail.com>
2026-02-25 15:35:35 -0500

    Init PlatformIO and create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#if defined(ARDUINO_AVR_UNO)
+
+#include "Arduino.h"
+
+void setup() {
+
+}
+
+void loop() {
+
+}
+
+#else
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!\n";
+}
+
+#endif
