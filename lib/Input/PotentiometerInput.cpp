c3fdba6c122463c1ea32f7df3455a1a8d31050e9
blankemeyer <blankemeyer.33@osu.edu>
2026-03-04 14:03:14 -0500

    create PotentiometerInput class

---
+++ lib/Input/PotentiometerInput.cpp
@@ -0,0 +1,13 @@
+#include "PotentiometerInput.h"
+
+#include <assert.h>
+
+#define MAX_QUANT 1023
+
+PotentiometerIntput::PotentiometerInput(IHAL hal, int pin) : m_hal{hal}, m_pin{pin} {}
+
+float PotentiometerInput::getValue() {
+	int quant = m_hal.analogRead(m_pin);
+
+	return (float)quant / (float)MAX_QUANT;
+}
--- lib/Input/PotentiometerInput.h
+++ lib/Input/PotentiometerInput.h
@@ -1,12 +1,27 @@
 #ifndef POTENTIOMETER_INPUT_H
 #define POTENTIOMETER_INPUT_H
 
+#include <IInput.h>
+
+#include <IHAL.h>
+
 /**
  * Potentiometer Input
- * 
+ *
  * Implementation of `IInput`. Has a reference to the HAL and a pin number.
  * Reads an analog potentiometer and provides normalized values. Arduino
  * analog inputs range from 0-1023, which are normalized to 0.0-1.0.
  */
+
+class PotentiometerInput : public IInput {
+public:
+	PotentiometerInput(IHAL hal, int pin);
+
+	float getValue() override;
+
+private:
+	IHAL m_hal;
+	int m_pin;
+};
 
 #endif // POTENTIOMETER_INPUT_H
