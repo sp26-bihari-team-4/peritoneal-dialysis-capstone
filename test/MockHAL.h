6964057991c8e53bd3f3467cfb1c16daa5c8a409
Dennis Zhitenev <denniszhitenev@gmail.com>
2026-03-11 17:19:35 -0400

    Update MockHAL to match IHAL

--- test/MockHAL.h
+++ test/MockHAL.h
@@ -3,10 +3,15 @@
 
 class MockHAL : public IHAL {
 public:
-	MOCK_METHOD(int, analogRead, (int pin), (override));
-	MOCK_METHOD(void, analogWrite, (int pin, int value), (override));
-	MOCK_METHOD(void, delay, (unsigned long ms), (override));
-	MOCK_METHOD(unsigned long, millis, (), (override));
-	MOCK_METHOD(int, getMaxAnalogReadQuant, (), (const, override));
-	MOCK_METHOD(int, getMaxAnalogWriteQuant, (), (const, override));
+	MOCK_METHOD(AnalogValue, analogRead, (int pin), (override));
+	MOCK_METHOD(void, analogWrite, (int pin, AnalogValue value), (override));
+	MOCK_METHOD(AnalogValue, getMaxAnalogReadQuant, (), (const, override));
+	MOCK_METHOD(AnalogValue, getMaxAnalogWriteQuant, (), (const, override));
+	MOCK_METHOD(DigitalValue, digitalRead, (int pin), (override));
+	MOCK_METHOD(void, digitalWrite, (int pin, DigitalValue value), (override));
+	MOCK_METHOD(void, beginSerial, (), (override));
+	MOCK_METHOD(size_t, print, (const char *s), (override));
+	MOCK_METHOD(size_t, println, (const char *s), (override));
+	MOCK_METHOD(void, delayMs, (unsigned long durationMs), (override));
+	MOCK_METHOD(unsigned long, getTimeMs, (), (override));
 };
