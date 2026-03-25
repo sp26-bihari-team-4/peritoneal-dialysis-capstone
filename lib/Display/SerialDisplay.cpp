c5628668a5e9041abd279387dc4cfb7633e63486
Dennis Zhitenev <denniszhitenev@gmail.com>
2026-03-25 17:59:47 -0400

    Write main.cpp and get program to compile
    Added a needed include to `CatheterController.h` and edited the implementation for `SerialDisplay::update` since Arduino doesn't support floats with `snprintf`.

--- lib/Display/SerialDisplay.cpp
+++ lib/Display/SerialDisplay.cpp
@@ -1,4 +1,5 @@
 #include <SerialDisplay.h>
+#include <math.h>
 #include <stdio.h>
 
 SerialDisplay::SerialDisplay(IHAL &hal) : hal(hal) {}
@@ -10,9 +11,9 @@ void SerialDisplay::initialize() {
 void SerialDisplay::update(float channel1Power, float channel2Power) {
 	snprintf(
 		string_buffer, sizeof(string_buffer),
-		"Wire 1 Power Level:\t%.0f%%\n"
-		"Wire 2 Power Level:\t%.0f%%\n",
-		channel1Power * 100, channel2Power * 100
+		"Wire 1 Power Level:\t%d%%\n"
+		"Wire 2 Power Level:\t%d%%\n",
+		(int)round(channel1Power * 100), (int)round(channel2Power * 100)
 	);
 
 	hal.println(string_buffer);
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
-#if defined(ARDUINO_AVR_UNO)
-
 #include <Arduino.h>
+#include <ArduinoHAL.h>
+#include <CatheterController.h>
+#include <SerialDisplay.h>
 
-void setup() {}
+#define INPUT_PIN_1 A0
+#define INPUT_PIN_2 A2
+#define OUTPUT_PIN_1 10
+#define OUTPUT_PIN_2 11
 
-void loop() {}
+#define BAUD_RATE 9600
+#define DELAY_MS 100
 
-#else
+ArduinoHAL hal(BAUD_RATE);
+SerialDisplay display(hal);
+CatheterController controller(
+	hal, INPUT_PIN_1, OUTPUT_PIN_1, INPUT_PIN_2, OUTPUT_PIN_2, display
+);
 
-#include <iostream>
+void setup() {
+	pinMode(INPUT_PIN_1, INPUT);
+	pinMode(INPUT_PIN_2, INPUT);
+	pinMode(OUTPUT_PIN_1, OUTPUT);
+	pinMode(OUTPUT_PIN_2, OUTPUT);
 
-int main() {
-	std::cout << "Hello, World!\n";
+	display.initialize();
 }
 
-#endif
+void loop() {
+	controller.update();
+	hal.delayMs(DELAY_MS);
+}
