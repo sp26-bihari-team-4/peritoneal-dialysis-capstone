d0b57ed59dac1daaa33caa4278c5c825c10954c9
blankemeyer <blankemeyer.33@osu.edu>
2026-04-02 23:26:28 -0400

    appease clang-tidy

--- src/AdafruitDisplay.cpp
+++ src/AdafruitDisplay.cpp
@@ -8,10 +8,10 @@ static constexpr uint8_t DISPLAY_REVISION{INITR_MINI160x80_PLUGIN};
 // Instantiate Adafruit driver using hardware SPI.
 
 AdafruitDisplay::AdafruitDisplay(int csPin, int dcPin)
-		// TODO: should we assert that 'csPin' and 'dcPin' are in range for
-		// 'int8_t'?
+		// TODO(will): should we assert that 'csPin' and 'dcPin' are in range for
+		//             'int8_t'?
 		: m_driver{
-				static_cast<int8_t>(csPin), static_cast<int8_t>(dcPin), /*rstPin=*/-1
+				static_cast<int8_t>(csPin), static_cast<int8_t>(dcPin), /*rst=*/-1
 			} {}
 
 void AdafruitDisplay::initializeDriver() {
@@ -39,7 +39,9 @@ void AdafruitDisplay::clearScreen(Color color) {
 }
 
 void AdafruitDisplay::drawText(Point topLeft, Color color, const char *string) {
-	m_driver.setCursor(topLeft.x, topLeft.y);
+	m_driver.setCursor(
+		static_cast<int16_t>(topLeft.x), static_cast<int16_t>(topLeft.y)
+	);
 	m_driver.setTextColor(encodeColor(color));
 	m_driver.print(string);
 }
@@ -50,5 +52,8 @@ AdafruitDisplay::Color565 AdafruitDisplay::encodeColor(Color color) {
 		return 0;
 	case Color::White:
 		return UINT16_MAX;
+	default:
+		// There are no other colors.
+		__builtin_unreachable();
 	}
 }
